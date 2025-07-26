3c844a8ba3842ba7c2574a4257df13f84d3b2684
milyasbpa <basilyas.2015@gmail.com>
2025-07-26 16:39:35 +0700

    fix: add faq

---
+++ src/pwa/features/home/components/faq_accordion/FAQAccordion.tsx
@@ -0,0 +1,62 @@
+import clsx from "clsx";
+import { Minus, Plus } from "lucide-react";
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+export interface FAQAccordionHomeProps {
+  question?: string;
+  answer?: string;
+}
+
+export const FAQAccordionHome = ({
+  question,
+  answer,
+}: FAQAccordionHomeProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const handleClickAccordionButton = () => {
+    setIsOpen((prev) => !prev);
+  };
+  return (
+    <div
+      className={clsx(
+        "grid grid-cols-1 place-content-start place-items-start",
+        isOpen ? "gap-[0.75rem]" : "gap-[0rem]",
+        "w-full",
+        "px-[2rem] py-[2rem]",
+        "rounded-[0.5rem]",
+        "bg-[#F0ECF64D]"
+      )}
+    >
+      <button
+        aria-label={question}
+        name={question}
+        className={clsx(
+          "grid grid-flow-col items-center content-center justify-between justify-items-start gap-[0rem]",
+          "w-full",
+          "cursor-pointer"
+        )}
+        onClick={handleClickAccordionButton}
+      >
+        <span className={clsx("text-[#37245B] text-[1.5rem] font-bold")}>
+          {question}
+        </span>
+        {isOpen ? (
+          <Minus className={clsx("w-[1.5rem] h-[1.5rem]", "text-[#472E75]")} />
+        ) : (
+          <Plus className={clsx("w-[1.5rem] h-[1.5rem]", "text-[#472E75]")} />
+        )}
+      </button>
+      <motion.div
+        initial={{ height: 0, opacity: 0 }}
+        animate={{
+          height: isOpen ? "auto" : 0,
+          opacity: isOpen ? 1 : 0,
+        }}
+        transition={{ duration: 0.3, ease: "easeInOut" }}
+        className={clsx("text-[#1D1D1D] text-[1.25rem] font-normal")}
+      >
+        {answer}
+      </motion.div>
+    </div>
+  );
+};
--- src/pwa/features/home/fragments/faq/FAQ.home.tsx
+++ src/pwa/features/home/fragments/faq/FAQ.home.tsx
@@ -1,10 +1,68 @@
 "use client";
+import { Badge } from "@/pwa/core/components/badge";
+import { useTranslation } from "@/pwa/core/i18n/hooks";
 import clsx from "clsx";
+import staticData from "@/pwa/features/home/data/static.json";
+import { FAQAccordionHome } from "../../components/faq_accordion";
 
 export const FAQHome = () => {
+  const { t } = useTranslation();
+  const items = staticData.faq.items.map((item) => {
+    return {
+      title: t(item.title),
+      description: t(item.description),
+    };
+  });
   return (
-    <div>
-      <div></div>
-    </div>
+    <section
+      className={clsx(
+        "grid grid-cols-1 items-center content-center justify-center justify-items-center gap-[1.5rem]",
+        "w-full",
+        "min-h-[1028px]"
+      )}
+    >
+      <div
+        className={clsx(
+          "grid grid-cols-1 items-center content-center justify-center justify-items-center gap-[3rem]",
+          "max-w-[1200px] w-full"
+        )}
+      >
+        <div
+          className={clsx(
+            "grid grid-cols-1 items-center content-center justify-center justify-items-center gap-[1rem]",
+            "w-full"
+          )}
+        >
+          <Badge>{t("faq:label")}</Badge>
+          <h2
+            className={clsx("text-[2.5rem] font-bold")}
+            style={{
+              background: "linear-gradient(90deg, #3A099C 0%, #10012D 100%)",
+              WebkitBackgroundClip: "text",
+              WebkitTextFillColor: "transparent",
+            }}
+          >
+            {t("faq:title")}
+          </h2>
+        </div>
+
+        <div
+          className={clsx(
+            "grid grid-cols-1 items-center content-center justify-center justify-items-center gap-[2rem]",
+            "w-full"
+          )}
+        >
+          {items.map((item, index) => {
+            return (
+              <FAQAccordionHome
+                key={index}
+                question={item.title}
+                answer={item.description}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </section>
   );
 };
