39c4ad8d2b20c260c244e6b74ece73d7c8df05c5
milyasbpa <basilyas.2015@gmail.com>
2025-07-29 16:22:25 +0700

    fix: risk free guarantee

--- src/pwa/features/home/fragments/risk_free_guarantee/RiskFreeGuarantee.home.tsx
+++ src/pwa/features/home/fragments/risk_free_guarantee/RiskFreeGuarantee.home.tsx
@@ -10,7 +10,8 @@ export const RiskFreeGuaranteeHome = () => {
       className={clsx(
         "grid grid-cols-1 items-center content-center justify-center justify-items-center gap-[1.5rem]",
         "w-full",
-        "min-h-[440px]"
+        "min-h-[260px] sm:min-h-[440px]",
+        "px-[1rem] sm:px-[0px]"
       )}
       style={{
         backgroundImage: "url('/images/home/risk_free_guarantee/bg.svg')",
@@ -24,17 +25,17 @@ export const RiskFreeGuaranteeHome = () => {
           "max-w-[1200px] w-full"
         )}
       >
-        <ShieldHalf className={clsx("w-[150px] h-[150px]", "text-[#472E75]")} />
+        <ShieldHalf className={clsx("w-[5rem] h-[5rem] sm:w-[150px] sm:h-[150px]", "text-[#472E75]")} />
         <div
           className={clsx(
             "grid grid-cols-1 items-center content-center justify-center justify-items-center gap-[1rem]",
             "w-full"
           )}
         >
-          <span className={clsx("text-[#2A1B45] text-[2.5rem] font-bold")}>
+          <span className={clsx("text-[#2A1B45] text-[1.125rem] sm:text-[2.5rem] font-bold text-center")}>
             {t("risk_free_guarantee:title")}
           </span>
-          <span className={clsx("text-[#2A1B45] text-[1.125rem] font-normal")}>
+          <span className={clsx("text-[#2A1B45] text-[0.75rem] sm:text-[1.125rem] font-normal text-center")}>
             {t("risk_free_guarantee:description")}
           </span>
         </div>
