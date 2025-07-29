8b212157db70a28e26ef79211ba135d5d8dd9e82
milyasbpa <basilyas.2015@gmail.com>
2025-07-29 16:03:06 +0700

    fix: testimonial card

--- src/pwa/features/home/fragments/testimonial/Testimonial.home.tsx
+++ src/pwa/features/home/fragments/testimonial/Testimonial.home.tsx
@@ -13,7 +13,8 @@ export const TestimonialHome = () => {
       className={clsx(
         "grid grid-cols-1 items-center content-center justify-center justify-items-center gap-[3rem]",
         "w-full",
-        "min-h-[648px]"
+        "min-h-[648px]",
+        "px-[1.5rem] md:px-0 py-[2rem]"
       )}
     >
       <div
@@ -30,7 +31,9 @@ export const TestimonialHome = () => {
         >
           <Badge>{t("testimonial:label")}</Badge>
           <h2
-            className={clsx("text-[2.5rem] font-bold text-left")}
+            className={clsx(
+              "text-[1.25rem] sm:text-[2.5rem] text-center sm:text-left font-bold"
+            )}
             style={{
               background: "linear-gradient(90deg, #3A099C 0%, #10012D 100%)",
               WebkitBackgroundClip: "text",
@@ -44,10 +47,10 @@ export const TestimonialHome = () => {
         {/* items */}
         <div
           className={clsx(
-            "grid place-content-center place-items-center gap-[2rem]",
-            "w-full"
+            "grid grid-flow-col place-content-start place-items-start gap-[2rem]",
+            "w-full",
+            "overflow-auto"
           )}
-          style={{ gridTemplateColumns: `repeat(4,1fr)` }}
         >
           {items.map((item, index) => (
             <TestimonialCardHome
