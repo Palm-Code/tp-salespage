53dcb9910f7746e57bd42775aa50280faa4b8be0
milyasbpa <basilyas.2015@gmail.com>
2025-07-30 12:15:43 +0700

    fix: background

--- src/pwa/features/home/fragments/what_you_get/WhatYouGet.home.tsx
+++ src/pwa/features/home/fragments/what_you_get/WhatYouGet.home.tsx
@@ -77,115 +77,125 @@ export const WhatYouGetHome = () => {
       className={clsx(
         "grid grid-cols-1 items-start content-start justify-center justify-items-center gap-[3rem]",
         "w-full",
-        "min-h-[1346px] md:min-h-[1196px]",
-        "px-[1.5rem] xl:px-0 py-[2rem]"
+        "min-h-[1346px] md:min-h-[1196px]"
       )}
       style={{
         background:
           "linear-gradient(180deg, rgba(255, 255, 255, 0.8) 0%, rgba(240, 236, 246, 0.8) 100%)",
-        backgroundImage: isMd
-          ? "url('/images/home/what_you_get/bg.svg')"
-          : "url('/images/home/what_you_get/bg_mobile.svg')",
         backgroundPosition: "center",
         backgroundSize: "cover",
       }}
     >
       <div
         className={clsx(
-          "grid grid-cols-1 items-start content-start justify-center justify-items-center gap-[3rem]",
-          "max-w-5xl w-full"
+          "grid grid-rows-1 grid-cols-1 items-start content-start justify-center justify-items-center gap-[3rem]",
+          "w-full h-full",
+          "px-[1.5rem] xl:px-0 py-[2rem]"
         )}
+        style={{
+          backgroundImage: isMd
+            ? "url('/images/home/what_you_get/bg.svg')"
+            : "url('/images/home/what_you_get/bg_mobile.svg')",
+          backgroundPosition: "center",
+        }}
       >
         <div
           className={clsx(
-            "grid grid-cols-1 items-start content-start justify-center justify-items-center gap-[0.5rem] md:gap-[1rem]",
-            "w-full"
+            "grid grid-cols-1 items-start content-start justify-center justify-items-center gap-[3rem]",
+            "max-w-5xl w-full h-full"
           )}
         >
-          <Badge>{t("what_you_get:label")}</Badge>
-          <h2
+          <div
             className={clsx(
-              "text-[1.25rem] md:text-[2.5rem] text-center md:text-left font-bold"
+              "grid grid-cols-1 items-start content-start justify-center justify-items-center gap-[0.5rem] md:gap-[1rem]",
+              "w-full"
             )}
-            style={{
-              background: "linear-gradient(90deg, #3A099C 0%, #10012D 100%)",
-              WebkitBackgroundClip: "text",
-              WebkitTextFillColor: "transparent",
-            }}
           >
-            {t(staticData.what_you_get.title)}
-          </h2>
-        </div>
-        {/* video */}
+            <Badge>{t("what_you_get:label")}</Badge>
+            <h2
+              className={clsx(
+                "text-[1.25rem] md:text-[2.5rem] text-center md:text-left font-bold"
+              )}
+              style={{
+                background: "linear-gradient(90deg, #3A099C 0%, #10012D 100%)",
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+              }}
+            >
+              {t(staticData.what_you_get.title)}
+            </h2>
+          </div>
+          {/* video */}
 
-        <video
-          ref={videoRef}
-          className={clsx(
-            "w-full h-[312px] md:h-[528px]",
-            "object-cover",
-            "rounded-[1.5rem]"
-          )}
-          autoPlay
-          loop
-          playsInline
-          preload="auto"
-          muted={isMuted}
-        >
-          <source
-            src={
-              "https://res.cloudinary.com/drccyjwrq/video/upload/f_auto:video,q_50/v1/videos/Tiny_Parrot_Product_Demo_-_English_Portrait_ngka4y"
-            }
-            type="video/mp4"
-            media="(max-width: 768px)"
-          />
-          <source
-            src={"/videos/what_you_get/video.webm"}
-            type="video/mp4"
-            media="(min-width: 769px)"
-          />
-        </video>
-
-        <motion.div
-          style={{
-            opacity: useTransform(
-              scrollYProgress,
-              [0.18, 0.25, 0.3, 0.45],
-              [0, 1, 1, 0]
-            ),
-          }}
-          className={"fixed bottom-12 right-0 z-[45] container"}
-        >
-          <button
+          <video
+            ref={videoRef}
             className={clsx(
-              "ml-auto",
-              "text-white backdrop-blur-[5px] p-[0.5rem] rounded-[0.5rem]"
+              "w-full h-[312px] md:h-[528px]",
+              "object-cover",
+              "rounded-[1.5rem]"
             )}
-            style={{
-              background:
-                "linear-gradient(90deg, rgba(20, 6, 45, 0.70) 0%, rgba(74, 70, 83, 0.70) 100%)",
-              boxShadow: "0px 0px 10px 0px #C9ABFF",
-            }}
-            onClick={() => setIsMuted(!isMuted)}
+            autoPlay
+            loop
+            playsInline
+            preload="auto"
+            muted={isMuted}
           >
-            {isMuted ? <VolumeOff /> : <Volume2 />}
-          </button>
-        </motion.div>
-
-        {/* items */}
-        <div
-          className={clsx(
-            "grid grid-cols-1 lg:grid-cols-3 place-content-center place-items-center gap-[3rem]",
-            "w-full"
-          )}
-        >
-          {items.map((item, index) => (
-            <WhatYouGetCardHome
-              key={index}
-              icon={item.icon}
-              title={item.title}
-              description={item.description}
+            <source
+              src={
+                "https://res.cloudinary.com/drccyjwrq/video/upload/f_auto:video,q_50/v1/videos/Tiny_Parrot_Product_Demo_-_English_Portrait_ngka4y"
+              }
+              type="video/mp4"
+              media="(max-width: 768px)"
             />
-          ))}
+            <source
+              src={"/videos/what_you_get/video.webm"}
+              type="video/mp4"
+              media="(min-width: 769px)"
+            />
+          </video>
+
+          <motion.div
+            style={{
+              opacity: useTransform(
+                scrollYProgress,
+                [0.18, 0.25, 0.3, 0.45],
+                [0, 1, 1, 0]
+              ),
+            }}
+            className={"fixed bottom-12 right-0 z-[45] container"}
+          >
+            <button
+              className={clsx(
+                "ml-auto",
+                "text-white backdrop-blur-[5px] p-[0.5rem] rounded-[0.5rem]"
+              )}
+              style={{
+                background:
+                  "linear-gradient(90deg, rgba(20, 6, 45, 0.70) 0%, rgba(74, 70, 83, 0.70) 100%)",
+                boxShadow: "0px 0px 10px 0px #C9ABFF",
+              }}
+              onClick={() => setIsMuted(!isMuted)}
+            >
+              {isMuted ? <VolumeOff /> : <Volume2 />}
+            </button>
+          </motion.div>
+
+          {/* items */}
+          <div
+            className={clsx(
+              "grid grid-cols-1 lg:grid-cols-3 place-content-center place-items-center gap-[3rem]",
+              "w-full"
+            )}
+          >
+            {items.map((item, index) => (
+              <WhatYouGetCardHome
+                key={index}
+                icon={item.icon}
+                title={item.title}
+                description={item.description}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </section>
